b036324281d1018782488efb2ab7273853b95508
LuukGubbels <61047730+LuukGubbels@users.noreply.github.com>
2023-03-10 10:41:56 +0100

    Update processing.py

--- processing.py
+++ processing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import sys, getopt
 sys.path.append('../')
-from Modules import thesis_module as tm
+import thesis_module as tm
 
 infile = "../randomSample_purified_data2.csv"
 
