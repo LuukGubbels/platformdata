c7ecbbd2f4a5dce71740aa63c74b078163c3cf06
LuukGubbels <61047730+LuukGubbels@users.noreply.github.com>
2023-03-10 13:59:06 +0100

    Add files via upload

---
+++ processing 2.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import sys, getopt
+sys.path.append('../')
+import thesis_module as tm
+
+infile = "../randomSample_purified_data2.csv"
+
+argv = sys.argv[1:]
+
+try:
+    opts, args = getopt.getopt(argv,"i",["infile="])
+except getopt.GetoptError:
+    sys.exit(2)
+if '?' in args or 'help' in args:
+    print('Help for "processing.py"')
+    print('This file is used to process data.')
+    print()
+    print('Options:')
+    print('-i, --infile:  Defines the file that requires processing.')
+    print()
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in ("-i","--infile"):
+        infile = arg.strip('][').split(',')
+
+frames = []
+for i in infile:
+    df = pd.read_csv(i, sep = ';')
+    df = df.fillna(" ")
+    df = df[df["text"],str.split().apply(len)>=10]
+    frames.append(df)
+df = pd.concat(frames, sort=True)
+df["text"].str.findall('w{3,}').str.join(' ')
+df["text"] = df["text"].str.replace("  ", " ")
+df =  df.drop(["platform"], axis=1)
+
+df.to_csv(infile[:-4]+' processed.csv')
