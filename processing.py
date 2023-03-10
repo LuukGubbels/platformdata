3cd09cc16e172520e8583b5048de602334e3fb2a
LuukGubbels <61047730+LuukGubbels@users.noreply.github.com>
2023-03-10 10:39:23 +0100

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,10 +1,27 @@
 import pandas as pd
 import numpy as np
-import sys
+import sys, getopt
 sys.path.append('../')
 from Modules import thesis_module as tm
 
 infile = "../randomSample_purified_data2.csv"
+
+try:
+  opts, args = getopt.getopt(argv,
+                             "i",["infile="])
+ except getopt.GetoptError:
+  sys.exit(2)
+if '?' in args or 'help' in args:
+  print('Help for "processing.py"')
+  print('This file is used to process data.')
+  print()
+  print('Options:')
+  print('-i, --infile:  Defines the file that requires processing.')
+  print()
+  sys.exit(2)
+for opt, arg in opts:
+  if opt in ("-i","--infile"):
+    infile = arg
 
 df = pd.read_csv(infile, sep = ';')
 result = tm.preprocess(infile)
