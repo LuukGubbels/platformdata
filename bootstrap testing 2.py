4989cad5ea73ecf44de36fc07449b2c3670122c2
LuukGubbels <61047730+LuukGubbels@users.noreply.github.com>
2023-04-06 11:31:43 +0200

    Update bootstrap testing 2.py

--- bootstrap testing 2.py
+++ bootstrap testing 2.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     X_test_pos = '../randomSample_purified_data2 processed.csv'
     X_test_neg = '../randomSample_purified_data2 processed.csv'
     outfile = '../Results/BootstrapResults.csv'
-    iters = 2
+    iters = 100
     jobs = 5
     feats = False
 
@@ -25,15 +25,14 @@ if __name__ == "__main__":
         print('Note that all input files should be processed by processed.py before using.')
         print()
         print('Options:')
-        print('-trp, --trpos: Defines the file from which positive training data should be read. Input as a .csv file with extension.')
-        print('-trn, --trneg: Defines the file from which negative training data should be read. Input as a .csv file with extension.')
-        print('-tep, --tepos: Defines the file from which positive testing data should be read. Input as a .csv file with extension.')
-        print('-ten, --teneg: Defines the file from which negative testing data should be read. Input as a .csv file with extension.')
-        # print('-i, --ifile:   Defines the files from which files should be read. Input as a python list with file extension.')
-        print('-o, --ofile:   Defines the file in which results should be stored. Input with a file extension.')
-        print('-n, --iters:   Defines the number of machines / bootstrap samples should be used. Non-integer numbers will be rounded down.')
-        print('-j, --jobs:    Defines the number of jobs should be used.')
-        print('-f, --feats:   Defines if features should be stored.')
+        print('-trp, --trpos: Defines the file from which positive training data should be read. Input as a .csv file with extension. Defaults to "data/X_train_pos processed.csv".')
+        print('-trn, --trneg: Defines the file from which negative training data should be read. Input as a .csv file with extension. Defaults to "data/X_train_neg processed.csv".')
+        print('-tep, --tepos: Defines the file from which positive testing data should be read. Input as a .csv file with extension. Defaults to "data/X_test_pos processed.csv".')
+        print('-ten, --teneg: Defines the file from which negative testing data should be read. Input as a .csv file with extension. Defaults to "data/X_test_neg processed.csv".')
+        print('-o, --ofile:   Defines the file in which results should be stored. Input with a file extension. Defaults to "../Results/BootstrapResults.csv".')
+        print('-n, --iters:   Defines the number of machines / bootstrap samples should be used. Non-integer numbers will be rounded down. Defaults to 100.')
+        print('-j, --jobs:    Defines the number of jobs should be used. Non-integer numbers will be rounded down. Defaults to 5.')
+        print('-f, --feats:   Defines if features should be stored. Defaults to False.')
 
         print()
         sys.exit(2)
@@ -79,6 +78,7 @@ from multiprocessing import Value, Process, Manager
 from time import time
 
 class Machine(Process):
+    # Initialize a process, storing the associated datasets and arrays for storing predictions
     def __init__(self, alg, id, X_train_pos, X_train_neg, 
                  X_test,
                  y_pred, y_predP, y_predC, y_predCP):
@@ -94,18 +94,13 @@ class Machine(Process):
         self.y_predCP = y_predCP
 
     def run(self):
+        
+        # Form the training data set and process it
         with open(self.X_train_pos) as f:
             tr_pos_n = sum(1 for line in f) - 1
         y_train_pos = np.ones(tr_pos_n)
         with open(self.X_train_neg) as f:
             tr_neg_n = sum(1 for line in f) - 1
-        # with open(self.X_test_pos) as f:
-        #     te_pos_n = sum(1 for line in f) - 1
-        # y_test_pos = np.ones(int(te_pos_n/10))
-        # with open(self.X_test_neg) as f:
-        #     te_neg_n = sum(1 for line in f) - 1
-        # y_test_neg = np.zeros(int(te_neg_n/10))
-        # y_test = np.concatenate([y_test_pos, y_test_neg])
         
         tr_pos = np.random.choice(range(1,tr_pos_n), size = tr_pos_n, replace = True)
         X_train_pos1 = pd.read_csv(self.X_train_pos)
@@ -113,88 +108,34 @@ class Machine(Process):
         
         tr_neg = np.random.choice(range(1,tr_neg_n), size = int((tr_pos_n-1)*7/3), replace = False)
         y_train_neg = np.zeros(len(tr_neg))
-#         tr_neg = np.random.choice(range(1,tr_neg_n), size = int(tr_neg_n/10), replace = False)
         tr_neg = np.concatenate([tr_neg, [0]])
         X_train_neg1 = pd.read_csv(self.X_train_neg, skiprows= lambda i: i not in tr_neg)
         X_train = pd.concat([X_train_pos1, X_train_neg1])
         y_train = np.concatenate([y_train_pos, y_train_neg])
-
-        # te_pos = np.random.choice(range(1,te_pos_n), size = int(te_pos_n/10), replace = False)
-        # te_pos = np.concatenate([te_pos,[0]])
-        # X_test_pos1 = pd.read_csv(self.X_test_pos, skiprows = lambda i: i not in te_pos)
-        # te_neg = np.random.choice(range(1,te_neg_n), size=int(te_neg_n/10), replace = False)
-        # te_neg = np.concatenate([te_neg,[0]])
-        # X_test_neg1 = pd.read_csv(self.X_test_neg, skiprows = lambda i: i not in te_neg)
-        # X_test = pd.concat([X_test_pos1, X_test_neg1])      
-
+    
         X_train, features, tfidfvectorizer, cv = tm.processing(X_train)
         features = np.concatenate([features,['language']])
         
+        # Fit the machine and store the features if needed
         self.alg.fit(X_train,y_train)
         if feats:
             features_w = np.vstack([features, self.alg.classifier.coef_[0]]).T
             pd.DataFrame(features_w, columns=["Features","Weights"]).to_csv('features/BootstrapFeats'+str(self.id)+'.csv')
 
+        # Process the test set and use the different predictions variants
         self.X_test = tm.processing(self.X_test, tfidfvectorizer=tfidfvectorizer, cv=cv)
         
         self.y_pred.append(self.alg.predict(self.X_test, new_threshold = False, cal = False))
         self.y_predP.append(self.alg.predict_proba(self.X_test, cal = False)[:,1])
         self.y_predC.append(self.alg.predict(self.X_test, new_threshold = False, cal = True))
-        self.y_predCP.append(self.alg.predict_proba(self.X_test, cal = True)[:,1])
-
-        # threshold = 0.5
-        # self.posest.value = np.sum(y_pred)
-        # [_, FP], [FN, _] = tm.confusion_est(y_test, y_pred)
-        # self.bias.value = (FP-FN)/len(y_test)
-        # self.sPCC.value = np.round(tm.rho(y_test, y_pred),4)
-        # self.acc.value = np.round(sklearn.metrics.accuracy_score(y_test, y_pred>=threshold), 4)            
-        # self.BA.value = np.round(sklearn.metrics.balanced_accuracy_score(y_test, y_pred>=threshold),4)
-        # self.MCC.value = np.round(sklearn.metrics.matthews_corrcoef(y_test, y_pred>=threshold),4)
-        # self.AUROC.value = np.round(sklearn.metrics.roc_auc_score(y_test, y_pred),4)
-
-        # y_pred = self.alg.predict_proba(X_test, cal = False)[:,1]
-        # self.posest_P.value = np.sum(y_pred)
-        # [_, FP], [FN, _] = tm.confusion_est(y_test, y_pred)
-        # self.bias_P.value = (FP-FN)/len(y_test)
-        # self.sPCC_P.value = np.round(tm.rho(y_test, y_pred),4)
-        # self.acc_P.value = np.round(sklearn.metrics.accuracy_score(y_test, y_pred>=threshold), 4)            
-        # self.BA_P.value = np.round(sklearn.metrics.balanced_accuracy_score(y_test, y_pred>=threshold),4)
-        # self.MCC_P.value = np.round(sklearn.metrics.matthews_corrcoef(y_test, y_pred>=threshold),4)
-        # self.AUROC_P.value = np.round(sklearn.metrics.roc_auc_score(y_test, y_pred),4)
-
-        # y_pred = self.alg.predict(X_test, new_threshold = False, cal = True)
-        # threshold = self.alg.threshold
-        # self.posest_C.value = np.sum(y_pred)
-        # [_, FP], [FN, _] = tm.confusion_est(y_test, y_pred)
-        # self.bias_C.value = (FP-FN)/len(y_test)
-        # self.sPCC_C.value = np.round(tm.rho(y_test, y_pred),4)
-        # self.acc_C.value = np.round(sklearn.metrics.accuracy_score(y_test, y_pred>=threshold), 4)            
-        # self.BA_C.value = np.round(sklearn.metrics.balanced_accuracy_score(y_test, y_pred>=threshold),4)
-        # self.MCC_C.value = np.round(sklearn.metrics.matthews_corrcoef(y_test, y_pred>=threshold),4)
-        # self.AUROC_C.value = np.round(sklearn.metrics.roc_auc_score(y_test, y_pred),4)
-
-        # y_pred = self.alg.predict_proba(X_test, cal = True)[:,1]
-        # self.posest_CP.value = np.sum(y_pred)
-        # [_, FP], [FN, _] = tm.confusion_est(y_test, y_pred)
-        # self.bias_CP.value = (FP-FN)/len(y_test)
-        # self.sPCC_CP.value = np.round(tm.rho(y_test, y_pred),4)
-        # self.acc_CP.value = np.round(sklearn.metrics.accuracy_score(y_test, y_pred>=threshold), 4)            
-        # self.BA_CP.value = np.round(sklearn.metrics.balanced_accuracy_score(y_test, y_pred>=threshold),4)
-        # self.MCC_CP.value = np.round(sklearn.metrics.matthews_corrcoef(y_test, y_pred>=threshold),4)
-        # self.AUROC_CP.value = np.round(sklearn.metrics.roc_auc_score(y_test, y_pred),4)       
+        self.y_predCP.append(self.alg.predict_proba(self.X_test, cal = True)[:,1]) 
 
 if __name__ == "__main__":
     start = time()
     alg = LogisticRegression()
     alg = bc.calibrator_binary(alg, density = 'test')
-
-    # with open(X_train_pos) as f:
-    #     tr_pos_n = sum(1 for line in f) - 1
-    # y_train_pos = np.ones(tr_pos_n)
-    # with open(X_train_neg) as f:
-    #     tr_neg_n = sum(1 for line in f) - 1
-    # y_train_neg = np.zeros(tr_neg_n)
-    # y_train = np.concatenate([y_train_pos, y_train_neg])
+    
+    # Form the test set
     with open(X_test_pos) as f:
         te_pos_n = sum(1 for line in f) - 1
     y_test_pos = np.ones(int(te_pos_n/10))
@@ -209,29 +150,8 @@ if __name__ == "__main__":
     te_neg = np.concatenate([te_neg,[0]])
     X_test_neg = pd.read_csv(X_test_neg, skiprows = lambda i: i not in te_neg)
     X_test = pd.concat([X_test_pos, X_test_neg])
-
-    # processes = []
-    # for i in range(iters):
-        # tr_pos = np.random.choice(range(1,tr_pos_n), size = tr_pos_n, replace = True)
-        # # tr_pos  =np.concatenate([tr_pos, [0]])
-        # X_train_pos1 = pd.read_csv(X_train_pos)
-        # X_train_pos1 = X_train_pos1.iloc[tr_pos-1]
-        
-        # # tr_neg = np.random.choice(range(1,tr_neg), size = int((len(tr_pos)-1)*7/3), replace = False)
-        # tr_neg = np.random.choice(range(1,tr_neg_n), size = int(tr_neg_n/10), replace = False)
-        # tr_neg = np.concatenate([tr_neg, [0]])
-        # X_train_neg1 = pd.read_csv(X_train_neg, skiprows= lambda i: i not in tr_neg)
-        # X_train = pd.concat([X_train_pos1, X_train_neg1])
-
-        # te_pos = np.random.choice(range(1,te_pos_n), size = int(te_pos_n/10), replace = False)
-        # te_pos = np.concatenate([te_pos,[0]])
-        # X_test_pos1 = pd.read_csv(X_test_pos, skiprows = lambda i: i not in te_pos)
-        # te_neg = np.random.choice(range(1,te_neg_n), size=int(te_neg_n/10), replace = False)
-        # te_neg = np.concatenate([te_neg,[0]])
-        # X_test_neg1 = pd.read_csv(X_test_neg, skiprows = lambda i: i not in te_neg)
-        # X_test = pd.concat([X_test_pos1, X_test_neg1])
-
     
+    # Initialize a manager to manage the prediction arrays, such that the processes can store their predictions
     man = Manager()
     y_pred = man.list()
     y_predP = man.list()
@@ -241,6 +161,8 @@ if __name__ == "__main__":
                                     X_test,
                                     y_pred, y_predP,
                                     y_predC, y_predCP) for i in range(iters)]
+    
+    # Execute the processes in batches and wait until they are finished to start the new batch
     batches = range(int((iters-1)/jobs+1))
     for i in tqdm(batches):
         for process in processes[i*jobs:(i+1)*jobs]:
@@ -249,6 +171,8 @@ if __name__ == "__main__":
             process.join()
         for process in processes[i*jobs:(i+1)*jobs]:
             process.terminate()
+            
+    # Store the metrics per variant and determine the average vote
     TP = np.sum(y_test)
     TP = np.array([TP,TP,TP,TP], dtype='float64')
     posest = np.array([0,0,0,0], dtype='float64')
@@ -298,26 +222,8 @@ if __name__ == "__main__":
     MCC[3] = sklearn.metrics.matthews_corrcoef(y_test,y_predCP >= 0.5)
     AUROC[3] = sklearn.metrics.roc_auc_score(y_test,y_predCP)    
 
-
-        # for process in processes[i*jobs:(i+1)*jobs]:
-        #     TP += np.array([process.TP.value, process.TP.value, process.TP.value, process.TP.value])
-        #     posest += np.array([process.posest.value, process.posest_P.value, process.posest_C.value, process.posest_CP.value])
-        #     bias += np.array([process.bias.value, process.bias_P.value, process.bias_C.value, process.bias_CP.value])
-        #     sPCC += np.array([process.sPCC.value, process.sPCC_P.value, process.sPCC_C.value, process.sPCC_CP.value])
-        #     acc += np.array([process.acc.value, process.acc_P.value, process.acc_C.value, process.acc_CP.value])
-        #     BA += np.array([process.BA.value, process.BA_P.value, process.BA_C.value, process.BA_CP.value])
-        #     MCC += np.array([process.MCC.value, process.MCC_P.value, process.MCC_C.value, process.MCC_CP.value])
-        #     AUROC += np.array([process.AUROC.value, process.AUROC_P.value, process.AUROC_C.value, process.AUROC_CP.value])
-        # size = len(processes[i*jobs:(i+1)*jobs])
-        # TP /= size
-        # posest /= size
-        # bias /= size
-        # sPCC /= size
-        # acc /= size
-        # BA /= size
-        # MCC /= size
-        # AUROC /= size
-
+    
+    # Store the metrics as results in a .csv
     dfmets = pd.DataFrame(columns= ['BayesCal?','Proba?',
                                 'TP','Pos. Est.','Bias','sPCC',
                                 'Acc','BA','MCC','AUROC'])
